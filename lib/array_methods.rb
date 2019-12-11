33c278eb12f633e93bae20c2f79166afeb97e041
darling-flag-8189 <dwinniford@gmail.com>
2019-12-11 20:18:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  puts array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+puts array.max.index
 end
 
 def find_min_value(array)
